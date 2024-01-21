c92b24eac4995f3f188168479cb5258428ef1bcb
Davide Di Francesco <davdifr@gmail.com>
2024-01-21 13:21:05 +0100

    Refactor TransactionService class

--- src/app/services/transaction.service.ts
+++ src/app/services/transaction.service.ts
@@ -1,26 +1,7 @@
-import { Injectable, signal } from '@angular/core';
+import { Injectable } from '@angular/core';
 import { FinancialTransaction } from '../models/transactions.models';
 
 @Injectable({
     providedIn: 'root',
 })
-export class TransactionService {
-    #transactions = signal<FinancialTransaction[]>([]);
-
-    addTransaction(transaction: FinancialTransaction): void {
-        this.#transactions.update((transactions) => [
-            ...transactions,
-            transaction,
-        ]);
-    }
-
-    removeTransaction(transactionId: string): void {
-        this.#transactions.update((transactions) =>
-            transactions.filter((t) => t.id !== transactionId)
-        );
-    }
-
-    getTransactions(): FinancialTransaction[] {
-        return this.#transactions();
-    }
-}
+export class TransactionService {}
