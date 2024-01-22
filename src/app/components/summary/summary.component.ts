7b3b9741a85dacf2c4be46155bc9124bfcaf5d00
Davide Di Francesco <davdifr@gmail.com>
2024-01-22 15:11:29 +0100

    Add summary component and update main component

---
+++ src/app/components/summary/summary.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input, input } from '@angular/core';
+
+@Component({
+    selector: 'app-summary',
+    standalone: true,
+    imports: [],
+    template: `{{ incomes() }} | {{ outcomes() }} | {{ total() }}`,
+})
+export class SummaryComponent {
+    incomes = input<number>();
+    outcomes = input<number>();
+    total = input<number>();
+}
--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { TransactionService } from '../../services/transaction.service';
+import { SummaryComponent } from '../../components/summary/summary.component';
 
 @Component({
     selector: 'app-main',
     standalone: true,
-    imports: [],
-    templateUrl: './main.component.html',
-    styleUrl: './main.component.css',
+    imports: [SummaryComponent],
+    template: `<app-summary
+        [incomes]="ts.incomes()"
+        [outcomes]="ts.outcomes()"
+        [total]="ts.total()"
+    /> `,
 })
-export default class MainComponent {}
+export default class MainComponent {
+    ts = inject(TransactionService);
+}
